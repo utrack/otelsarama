8ca1486eacf84110289988d9a808d9789af7816b
Nikita Koptelov <nick@koptelov.me>
2026-09-10 18:56:01 +0200

    otel semconv v1.43.0

--- dispatcher.go
+++ dispatcher.go
@@ -23,7 +23,7 @@ import (
 	"github.com/IBM/sarama"
 
 	"go.opentelemetry.io/otel/attribute"
-	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.43.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
--- producer.go
+++ producer.go
@@ -26,7 +26,7 @@ import (
 	"go.opentelemetry.io/otel/codes"
 
 	"go.opentelemetry.io/otel/attribute"
-	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.43.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
